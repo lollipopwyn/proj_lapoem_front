aea9eff57600a31328b2a4dac9ea23d727f89432
lollipopwyn <rwewine@gmail.com>
2024-11-02 14:29:18 +0900

    241029_fix_booklist_북리스트프론트에 연결된 api주소 수정_나라

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
-import Main from "./components/Main/Main";
-import Join from "./components/Auth/Join";
-import Login from "./components/Auth/Login";
-import Booklist from "./components/BookList/Booklist";
-import Stella from "./components/Stella/Stella";
-import Community from "./components/Community/Community";
-import NewForum from "./components/Community/NewForum";
-import Community_detail from "./components/Community/Community_detail";
-import ThreadOn from "./components/ThreadOn/Threadon";
+import Navbar from './components/NavBar';
+import Main from './components/Main/Main';
+import Join from './components/Auth/Join';
+import Login from './components/Auth/Login';
+import Booklist from './components/BookList/Booklist';
+import Stella from './components/Stella/Stella';
+import Community from './components/Community/Community';
+import NewForum from './components/Community/NewForum';
+import Community_detail from './components/Community/Community_detail';
+import ThreadOn from './components/ThreadOn/Threadon';
 
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { initializeAuth } from "./redux/features/auth/authSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { initializeAuth } from './redux/features/auth/authSlice';
 
 function App() {
   const dispatch = useDispatch();
@@ -31,7 +31,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/join" element={<Join />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/booklist" element={<Booklist />} />
+          <Route path="/book-list" element={<Booklist />} />
           <Route path="/chatstella" element={<Stella />} />
           <Route path="/thread_on" element={<ThreadOn />} />
           <Route path="/community" element={<Community />} />

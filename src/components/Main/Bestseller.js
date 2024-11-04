a0ef19e1fd6c946937d464c5c579a1462013775a
sora <ju22hy@gmail.com>
2024-11-04 20:14:12 +0900

    241104_feat_main_베스트셀러 데이터 가져오기 및 도서 영역 css 통합_유소라

--- src/components/Main/Bestseller.js
+++ src/components/Main/Bestseller.js
@@ -1,21 +1,49 @@
-import React from "react";
+// Bestseller.js
+import React, { useEffect, useState } from "react";
 import BookCard from "../Bookcard";
-import "./bestseller.css";
+import "./booksection.css"; // 공통 CSS 파일
+import { GET_BEST_BOOK_API_URL } from "../../util/apiUrl";
 
-function NewReleases() {
+function Bestseller() {
+  const [bestBooks, setBestBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBestBooks = async () => {
+      try {
+        const response = await fetch(GET_BEST_BOOK_API_URL);
+        if (!response.ok) throw new Error("Failed to fetch best books");
+
+        const data = await response.json();
+        setBestBooks(data);
+      } catch (error) {
+        console.error("Error fetching best books:", error);
+      }
+    };
+
+    fetchBestBooks();
+  }, []);
+
   return (
-    <section className="new-container">
-      <div className="new-title">
-        <h2>SPOTLIGHT ON NEW RELEASES</h2>
-        <p>새로 나온 주목할만한 작품들을 만나보세요 ✨</p>
+    <section className="book-section best">
+      <div className="section-title">
+        <h2>MONTHLY BEST SELLERS</h2>
+        <p>월간 베스트 셀러! 화제가 된 작품들을 만나보세요 📚</p>
       </div>
-      <div className="book_list flex gap-10">
-        {Array.from({ length: 5 }).map((_, idx) => (
-          <BookCard key={idx} />
+      <div className="book-list">
+        {bestBooks.map((book, idx) => (
+          <BookCard
+            key={idx}
+            thumbnail={book.book_cover}
+            title={book.book_title}
+            author={book.book_author}
+            publisher={book.book_publisher}
+            rating={book.average_rating}
+            reviewCount={book.review_count}
+          />
         ))}
       </div>
     </section>
   );
 }
 
-export default NewReleases;
+export default Bestseller;

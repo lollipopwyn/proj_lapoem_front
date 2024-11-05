4797994466487c40fc67bbcb8c0244d6c2235447
sora <ju22hy@gmail.com>
2024-11-05 20:33:52 +0900

    241105_fix_ThreadOn_스레드 post 페이지 검색 해결_유소라

--- src/components/ThreadOn/Threadon_post.js
+++ src/components/ThreadOn/Threadon_post.js
@@ -15,35 +15,24 @@ const Threadon_post = () => {
   const [books, setBooks] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalBooks, setTotalBooks] = useState(0);
-  const [limit] = useState(5); // 페이지당 표시할 책의 수
+  const [limit] = useState(5); // 페이지당 표시할 책의 수, 검색 시에도 5개로 제한
   const [loading, setLoading] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [isSearching, setIsSearching] = useState(false); // 검색 상태인지 여부
-  const [searchTerm, setSearchTerm] = useState(""); // 검색어 상태 저장
 
   useEffect(() => {
-    fetchBooks(currentPage, selectedCategory, searchTerm);
-  }, [currentPage, selectedCategory, searchTerm]);
+    fetchBooks(currentPage, selectedCategory);
+  }, [currentPage, selectedCategory]);
 
-  const fetchBooks = async (page, genre_tag_id, searchQuery = "") => {
+  const fetchBooks = async (page, genre_tag_id) => {
     setLoading(true);
     try {
-      let apiUrl;
-      const params = { page, limit };
-
-      if (isSearching && searchQuery) {
-        apiUrl = GET_SEARCH_BOOKS_API_URL;
-        params.keyword = searchQuery;
-        params.limit = limit; // 검색 결과의 도서 수 제한
-      } else if (genre_tag_id) {
-        apiUrl = GET_BOOK_BY_CATEGORY_API_URL;
-        params.genre_tag_id = genre_tag_id;
-      } else {
-        apiUrl = GET_BOOK_LIST_API_URL;
-      }
+      const apiUrl = genre_tag_id
+        ? GET_BOOK_BY_CATEGORY_API_URL
+        : GET_BOOK_LIST_API_URL;
+      const params = { page, limit, genre_tag_id }; // limit을 항상 추가
 
       const response = await axios.get(apiUrl, { params });
-      setBooks(response.data.data.slice(0, limit));
+      setBooks(response.data.data.slice(0, limit)); // 항상 limit만큼 데이터 표시
       setTotalBooks(response.data.totalBooks);
       setLoading(false);
     } catch (error) {
@@ -52,20 +41,17 @@ const Threadon_post = () => {
     }
   };
 
+  // 검색 결과를 처리하는 함수
   const handleSearch = (data) => {
-    setIsSearching(true);
-    setSearchTerm(data.keyword); // SearchBar에서 검색어를 가져와서 저장
-
     setBooks(data.data.slice(0, limit)); // 검색 결과 중 상위 5개만 도서 목록 업데이트
     setTotalBooks(data.totalBooks); // 총 도서 수 업데이트
-    setCurrentPage(1);
+    setCurrentPage(1); // 검색 시 페이지를 1로 초기화
     setSelectedCategory(""); // 검색 시 선택된 카테고리 초기화
   };
 
+  // 카테고리 선택 핸들러
   const handleCategoryChange = (category) => {
     setSelectedCategory(category); // 선택한 카테고리 ID로 상태 업데이트
-    setIsSearching(false); // 카테고리 필터링 시 검색 모드 해제
-    setSearchTerm(""); // 검색어 초기화
     setCurrentPage(1); // 필터 변경 시 페이지를 1로 초기화
   };
 

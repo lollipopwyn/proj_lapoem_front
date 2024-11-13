80b2436d1b35674022f613fa5d741f30651ecb4a
bonse koo <bonse88@hs.ac.kr>
2024-11-13 11:49:01 +0900

    241113_chore_SearchBar_UI수정_구본세

--- src/components/Common/SearchBar.js
+++ src/components/Common/SearchBar.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-import "./Common.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import './Common.css';
+import SerchIcon from '../../assets/images/searchIcon.png';
 
 const SearchBar = ({ apiUrl, onSearch }) => {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState('');
 
   const handleSearch = async () => {
-    if (keyword.trim() === "") return;
+    if (keyword.trim() === '') return;
 
     try {
       const response = await axios.get(apiUrl, {
@@ -15,7 +16,7 @@ const SearchBar = ({ apiUrl, onSearch }) => {
       });
       onSearch(response.data, keyword); // 검색어와 함께 검색 결과 전달
     } catch (error) {
-      console.error("Error fetching search results:", error);
+      console.error('Error fetching search results:', error);
     }
   };
 
@@ -25,9 +26,12 @@ const SearchBar = ({ apiUrl, onSearch }) => {
         type="text"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
-        placeholder="책 제목 또는 저자 검색..."
+        placeholder="검색할 도서명 혹은 저자를 입력해주세요."
       />
-      <button onClick={handleSearch}>검색</button>
+      <div className="divider"></div>
+      <button onClick={handleSearch}>
+        <img src={SerchIcon} alt="search_icon" className="search_icon"></img>
+      </button>
     </div>
   );
 };
